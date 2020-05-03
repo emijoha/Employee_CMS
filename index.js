3ceee1804799955da1fef74567acac94f1e6f4ca
emi <ejhartline@gmail.com>
2020-05-03 17:23:12 -0400

    Main inquirer prompts set up, with case switches for each user choice.

--- index.js
+++ index.js
@@ -7,17 +7,122 @@ connection.connect(function(err) {
     if (err) throw err;
     
     console.log("Connected to company_DB");
-    // getAction()
+    startAction();
 });
 
-function getAction() {
+// Function to initiate CLI app, runs once connection to database is successful
+function startAction() {
     inquirer
         .prompt({
-            // Get user's action choice
-            name: "action",
+            // Get user's action choice by category or exit
+            name: "startAction",
             type: "list",
             message: "What would you like to do?",
-            choices: []
+            choices: ["Manage 'Employees'", "Manage 'Roles'", "Manage 'Departments'", "Exit"]
+        }).then(function(answer) {
+            switch (answer.startAction) {
+            case "Manage 'Employees'":
+                employeeAction();
+                break;
 
-        })
+            case "Manage 'Roles'":
+                roleAction();
+                break;
+
+            case "Manage 'Departments'":
+                departmentAction();
+                break;
+
+            case "Exit":
+                console.log("Goodbye!")
+                connection.end();
+                break;
+            }
+        });
 };
+
+// Function to get user's action for employee data
+function employeeAction() {
+    inquirer
+        .prompt({
+            name: "employeeAction",
+            type: "list",
+            message: "What would you like to do with 'Employees'?",
+            choices: ["View All Employees", "Add Employee", "Update Employee Role", "Exit 'Employees'"]
+        }).then(function(answer) {
+            switch (answer.employeeAction) {
+                case "View All Employees":
+                    // viewEmployees();
+                    console.log("'View All Employees' selected");
+                    break;
+    
+                case "Add Employee":
+                    // addEmployee();
+                    console.log("Employee Added");
+                    break;
+    
+                case "Update Employee Role":
+                    //updateRole;
+                    console.log("Employee role updated");
+                    break;
+    
+                case "Exit 'Employees'":
+                    startAction();
+                    break;
+                }
+        });
+};
+
+// Function to get user's action for role data
+function roleAction() {
+    inquirer
+        .prompt({
+            name: "roleAction",
+            type: "list",
+            message: "What would you like to do with 'Roles'?",
+            choices: ["View All Roles", "Add Role", "Exit 'Roles'"]
+        }).then(function(answer) {
+            switch (answer.roleAction) {
+                case "View All Roles":
+                    // viewRoles();
+                    console.log("'View All Roles' selected");
+                    break;
+    
+                case "Add Role":
+                    // addRole();
+                    console.log("Role added");
+                    break;
+    
+                case "Exit 'Roles'":
+                    startAction();
+                    break;
+                }
+        });
+}
+
+// Function to get user's action for department data
+function departmentAction() {
+    inquirer
+        .prompt({
+            name: "departmentAction",
+            type: "list",
+            message: "What would you like to do with 'Departments'?",
+            choices: ["View All Departments", "Add Department", "Exit 'Departments'"]
+        }).then(function(answer) {
+            switch (answer.departmentAction) {
+                case "View All Departments":
+                    // viewRoles();
+                    console.log("'View All Departments' selected");
+                    break;
+    
+                case "Add Department":
+                    // addRole();
+                    console.log("Department added")
+                    break;
+    
+                case "Exit 'Departments'":
+                    startAction();
+                    break;
+                }
+        });
+}
